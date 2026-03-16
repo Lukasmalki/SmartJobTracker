a5ad802bdf33f4f0d223779428a8417b68f1bc69
Lukasmalki <lukasmalki@hotmail.com>
2026-03-16 23:53:53 +0100

    Initialized migrations and added connection string

---
+++ SmartJobBackend/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using SmartJobBackend.Models;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace SmartJobBackend
+{
+	public class AppDbContext : DbContext
+	{
+		public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+
+		public DbSet<JobApplication> JobApplications { get; set; }
+	}
+}
