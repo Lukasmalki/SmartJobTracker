2618c83b529377e44ad599575267c5b1e28e137b
Lukasmalki <lukasmalki@hotmail.com>
2026-03-23 19:43:59 +0100

    feat: implement JWT authentication and Swagger security definition in Program.cs

--- SmartJobBackend/Program.cs
+++ SmartJobBackend/Program.cs
@@ -1,8 +1,12 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Tokens;
 using Npgsql.EntityFrameworkCore.PostgreSQL;
+using System.Text;
+using Microsoft.OpenApi.Models;
 
 namespace SmartJobBackend
 {
@@ -14,7 +18,34 @@ namespace SmartJobBackend
 
 			builder.Services.AddControllers();
 			builder.Services.AddEndpointsApiExplorer();
-			builder.Services.AddSwaggerGen();
+
+			builder.Services.AddSwaggerGen(options =>
+			{
+				options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+				{
+					Name = "Authorization",
+					Type = SecuritySchemeType.Http,
+					Scheme = "bearer",
+					BearerFormat = "JWT",
+					In = ParameterLocation.Header,
+					Description = "Skriv: Bearer {din token}"
+				});
+
+				options.AddSecurityRequirement(new OpenApiSecurityRequirement
+	{
+		{
+			new OpenApiSecurityScheme
+			{
+				Reference = new OpenApiReference
+				{
+					Type = ReferenceType.SecurityScheme,
+					Id = "Bearer" // exakt match med AddSecurityDefinition
+                }
+			},
+			new string[] { }
+		}
+	});
+			});
 
 			builder.Services.AddCors(options =>
 			{
@@ -27,6 +58,20 @@ namespace SmartJobBackend
 			builder.Services.AddDbContext<AppDbContext>(options =>
 			options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+			builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+			{
+				options.TokenValidationParameters = new TokenValidationParameters
+				{
+					ValidateIssuer = true,
+					ValidateAudience = true,
+					ValidateLifetime = true,
+					ValidateIssuerSigningKey = true,
+					ValidIssuer = builder.Configuration["Jwt:Issuer"],
+					ValidAudience = builder.Configuration["Jwt:Audience"],
+					IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
+				};
+			});
+
 			var app = builder.Build();
 
 			if (app.Environment.IsDevelopment())
@@ -37,7 +82,10 @@ namespace SmartJobBackend
 
 			app.UseHttpsRedirection();
 			app.UseCors("AllowReact");
+
+			app.UseAuthentication();
 			app.UseAuthorization();
+
 			app.MapControllers();
 
 			app.Run();
