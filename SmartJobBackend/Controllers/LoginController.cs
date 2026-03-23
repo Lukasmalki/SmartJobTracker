a9eb632f6953904e207da21ac8d3c3c617ece7b2
Lukasmalki <lukasmalki@hotmail.com>
2026-03-23 19:39:05 +0100

    feat: add LoginController for user authentication and ProductController for product retrieval

---
+++ SmartJobBackend/Controllers/LoginController.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using SmartJobBackend.Models;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace SmartJobBackend.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class LoginController : ControllerBase
+	{
+		private IConfiguration _config;
+
+		public LoginController(IConfiguration config)
+		{
+			_config = config;
+		}
+
+		[AllowAnonymous]
+		[HttpPost]
+		public IActionResult Login([FromBody] User login)
+		{
+			IActionResult response = Unauthorized();
+			var user = AuthenticateUser(login);
+			if (user != null)
+			{
+				var tokenString = GenerateJSONWebToken(user);
+				response = Ok(new { token = tokenString });
+			}
+			return response;
+		}
+
+		private User AuthenticateUser(User login)
+		{
+			User user = null;
+
+			if (login.Username.ToLower() == "luki")
+			{
+				user = new User { Username = login.Username, Email = login.Email };
+			}
+			return user;
+		}
+
+		private string GenerateJSONWebToken(User userInfo)
+		{
+			var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Key"]));
+			var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+			var claims = new[]
+			{
+				new Claim(JwtRegisteredClaimNames.Sub, userInfo.Username),
+				new Claim(JwtRegisteredClaimNames.Email, userInfo.Email),
+				new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+			};
+
+			var token = new JwtSecurityToken(_config["Jwt:Issuer"],
+			  _config["Jwt:Issuer"],
+			  claims,
+			  expires: DateTime.Now.AddMinutes(30),
+			  signingCredentials: credentials);
+
+			return new JwtSecurityTokenHandler().WriteToken(token);
+		}
+	}
+}
---
+++ SmartJobBackend/Controllers/ProductController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SmartJobBackend.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class ProductController : ControllerBase
+	{
+		[Authorize]
+		[HttpGet]
+		public IActionResult GetProducts()
+		{
+			return Ok(new[] { "Product1", "Product2" });
+		}
+	}
+}
